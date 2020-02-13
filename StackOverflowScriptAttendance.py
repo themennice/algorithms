b1f107b0c218c3045e7371f28c5386adacccb61f
themennice <dziubiiden@gmail.com>
2020-02-14 00:27:43 +0800

    adding a starting edit of the automated script

---
+++ StackOverflowScriptAttendance.py
@@ -0,0 +1,7 @@
+import webbrowser
+
+webbrowser.open('https://stackoverflow.com/')
+webbrowser.open('https://www.hackerrank.com/')
+webbrowser.open('https://github.com/')
+webbrowser.open('https://leetcode.com/')
+print('Hello, World!')
