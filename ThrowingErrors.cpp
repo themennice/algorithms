7471bec6fffe0b7835ca133d868b29de6d1b38d4
themennice <dziubiiden@gmail.com>
2020-02-15 00:31:28 +0800

    adding throwing errors solution

---
+++ ThrowingErrors.cpp
@@ -0,0 +1,45 @@
+/*
+https://www.hackerrank.com/challenges/30-more-exceptions/
+*/
+
+#include <cmath>
+#include <iostream>
+#include <exception>
+#include <stdexcept>
+using namespace std;
+
+class Calculator
+{
+    private:
+        class e: public exception {
+            virtual const char* what() const throw() {
+                return "n and p should be non-negative";
+            }
+        } exp;
+
+    public:
+    int power(int n, int p)
+    {
+        if(n < 0 || p < 0)
+            throw exp;//runtime_error("n and p should be non-negative");
+        return pow(n,p);
+    }
+};
+int main()
+{
+    Calculator myCalculator=Calculator();
+    int T,n,p;
+    cin>>T;
+    while(T-->0){
+      if(scanf("%d %d",&n,&p)==2){
+         try{
+               int ans=myCalculator.power(n,p);
+               cout<<ans<<endl; 
+         }
+         catch(exception& e){
+             cout<<e.what()<<endl;
+         }
+      }
+    }
+    
+}
