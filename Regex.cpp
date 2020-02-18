6fce7c641fae9e4439a28369e2be83ed2986037c
themennice <dziubiiden@gmail.com>
2020-02-18 19:21:58 +0800

    adding regex solution

---
+++ Regex.cpp
@@ -0,0 +1,37 @@
+/*
+https://www.hackerrank.com/challenges/30-regex-patterns/problem
+*/
+#include <regex>
+#include <iostream>
+#include <string>
+#include <limits>
+
+using namespace std;
+
+int main()
+{
+    int N;
+    string userInput;
+    getline(cin, userInput);
+    N = stoi(userInput);
+    N = 6;
+
+    vector<string> gmail;
+
+    for (int N_itr = 0; N_itr < N; N_itr++) {
+
+        string firstName, emailID;
+        cin >> firstName;
+        cin >> emailID;
+        //cout << "Email ID is " << firstName << endl; 
+        regex e(".+@gmail\\.com$");
+        // if(emailID[emailID.length() - 9] == 'g' && emailID[emailID.length() - 10] == '@' ){
+        if(regex_match(emailID,e)){
+            gmail.push_back(firstName);
+        }
+    }
+    sort(gmail.begin(), gmail.end());
+    for(int i = 0; i < gmail.size(); i++)
+        cout << gmail[i] << endl;
+    return 0;
+}
