35f66a70e2fe953b209b588b923c44d32579e060
themennice <dziubiiden@gmail.com>
2021-01-12 19:44:25 -0800

    adding range sum bst easy solution

---
+++ RangeSumBST.cpp
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/range-sum-of-bst/
+// Time complexity: O(n) as we have to go through each tree node in the worst-case scenario
+// Space complexity: O(1) no new nodes are created
+
+int rangeSumBST(TreeNode* root, int low, int high) {
+    if(root == NULL) return 0;
+    if(root->val < low) return rangeSumBST(root->right, low, high);
+    if(root->val > high) return rangeSumBST(root->left, low, high);
+    return root->val + rangeSumBST(root->left, low, high) + rangeSumBST(root->right, low, high);
+}
