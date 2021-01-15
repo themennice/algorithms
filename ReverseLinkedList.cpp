a09c1a70142fae88d8065cb36ede9280e36833e7
themennice <dziubiiden@gmail.com>
2021-01-14 22:45:47 -0800

    adding reverse ll easy solution

---
+++ ReverseLinkedList.cpp
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/reverse-linked-list/
+
+ListNode* reverseList(ListNode* head) {
+    ListNode* prev = NULL;
+    ListNode* curr = head;
+    ListNode* next = NULL;
+    while(curr != NULL)
+    {
+        next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = next;
+    }
+    return prev;
+}
