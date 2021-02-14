b500be0101e02a8cabc56e64762d836875118df3
themennice <dziubiiden@gmail.com>
2021-02-13 20:08:13 -0800

    adding generate strings with odd chars easy solutions updates

--- GenerateStringWithOddChars.cpp
+++ GenerateStringWithOddChars.cpp
@@ -2,13 +2,15 @@
 // O(n) time and space
 
 string generateTheString(int n) {
-    string out = "";
-    bool isOdd = n % 2;
-    for(int i = 0; i < n; i++)
-    {
-        out += 'a';
-    }
-    if(!isOdd)
-        out[n - 1] = 'b';
-    return out;
-}   
+    // string out = "";
+    // bool isOdd = n % 2;
+    // for(int i = 0; i < n; i++)
+    // {
+    //     out += 'a';
+    // }
+    // if(!isOdd)
+    //     out[n - 1] = 'b';
+    // return out;
+    // return n % 2 ? string(n, 'a') : string(n - 1, 'a') + 'b';
+    return "b" + string(n - 1, 'a' + n % 2);   
+}
