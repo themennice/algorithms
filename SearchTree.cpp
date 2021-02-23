64e05389d93c5bfc4be869373c4efb63c9a22dd1
themennice <dziubiiden@gmail.com>
2021-02-22 22:30:59 -0800

    adding search tree easy solution

---
+++ SearchTree.cpp
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/increasing-order-search-tree
+
+TreeNode* increasingBST(TreeNode* root, TreeNode* tail = NULL) {
+ if (!root) return tail;
+    TreeNode* res = increasingBST(root->left, root);
+    root->left = NULL;
+    root->right = increasingBST(root->right, tail);
+    return res;   
+}
