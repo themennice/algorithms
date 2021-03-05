0976f7c2a3670975cdcd5be45b425eb6071a35e7
themennice <dziubiiden@gmail.com>
2021-03-05 13:51:30 -0800

    adding str str easy solution

---
+++ StrStr.cpp
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/implement-strstr
+
+int strStr(string haystack, string needle) {
+    int m = haystack.size(), n = needle.size(), p = 0;
+    while (p + n - 1 < m) {
+        if (haystack.substr(p, n) == needle) {
+            return p;
+        }
+        while (p++ + n - 1 < m && haystack[p] != needle[0]);
+    }
+    return -1;
+}
