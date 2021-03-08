e6f0d337ef0637311244b55dcebacba2c2da9701
themennice <dziubiiden@gmail.com>
2021-03-07 21:51:04 -0800

    adding x of a kind easy solution

---
+++ XOfAKind.cpp
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/x-of-a-kind-in-a-deck-of-cards/
+// O(n) time and space
+
+bool hasGroupsSizeX(vector<int>& deck) {
+    unordered_map<int, int> count;
+    int res = 0;
+    for (int i : deck) count[i]++;
+    for (auto i : count) res = __gcd(i.second, res);
+    return res > 1;
+}
