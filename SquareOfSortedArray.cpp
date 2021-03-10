d09e180c760922aaa85ff64a8d67b223333b7dd5
themennice <dziubiiden@gmail.com>
2021-03-09 22:16:08 -0800

    adding square of sorted array easy solution

---
+++ SquareOfSortedArray.cpp
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/squares-of-a-sorted-array
+// O(n) time and space complexity since each element is visited once and added to the output array
+
+vector<int> sortedSquares(vector<int>& nums) {
+    vector<int> res(nums.size());
+    int l = 0, r = nums.size() - 1;
+    for (int k = nums.size() - 1; k >= 0; k--) {
+        if (abs(nums[r]) > abs(nums[l])) res[k] = nums[r] * nums[r--];
+        else res[k] = nums[l] * nums[l++];
+    }
+    return res;
+}
