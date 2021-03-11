d0cf54001e8d3e910a95480e5241faf272bac71f
themennice <dziubiiden@gmail.com>
2021-03-10 20:20:15 -0800

    adding unique number of occurrences easy solution

---
+++ UniqueNumberOfOccurrences.cpp
@@ -0,0 +1,11 @@
+// https://leetcode.com/problems/unique-number-of-occurrences
+// O(n) time and space
+
+bool uniqueOccurrences(vector<int>& arr) {
+  unordered_map<int, int> m;
+  unordered_set<int> s;
+  for (auto n : arr) ++m[n];
+  for (auto& p : m)
+      if (!s.insert(p.second).second) return false;
+  return true;
+}
